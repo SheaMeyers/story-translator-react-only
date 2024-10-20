b25b6cc96ebceff46680245dee2e8d957a7cee5a
Shea Meyers <shea.meyers@gmail.com>
2024-10-20 15:50:30 +0200

    Display specific language text from pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import { useState } from 'react';
+import aliceInWonderland from './translations/aliceInWonderland.json';
+import theCantervilleGhost from './translations/theCantervilleGhost.json';
 import './App.css';
 
-function App() {
+type Book = {
+  title: 'Alice in Wonderland',
+  file: 'aliceInWonderland'
+} | {
+  title: 'The Canterville Ghost',
+  file: 'theCantervilleGhost'
+}
+
+
+type Language = 'English' | 'Spanish' | 'French' | 'German'
+
+type Paragraph = {
+  English: string
+  Spanish: string
+  French: string
+  German: string
+}
+
+
+const App = () => {
+  const [bookJson, setBookJson] = useState<Paragraph[]>(aliceInWonderland)
+  const [page, setPage] = useState<number>(0)
+  const [textLanguage, setTextLanguage] = useState<Language | null>('English')
+  const [popUpLanguage, setPopupLanguageLanguage] = useState<Language | null>('French')
+  
+
   return (
     <div className="App">
       <h1>Story translator</h1>
+      {textLanguage && popUpLanguage && 
+      <>
+        <p>Text Language: {bookJson[page][textLanguage]}</p>
+        <p>Popup Language: {bookJson[page][popUpLanguage]}</p>
+      </>
+      }
     </div>
   );
 }
