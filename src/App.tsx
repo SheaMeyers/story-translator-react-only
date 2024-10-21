04c4a23b898fe38800b799b3a9715cf69783510b
Shea Meyers <shea.meyers@gmail.com>
2024-10-21 20:32:29 +0200

    Show popup translation text

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react';
-import aliceInWonderland from './translations/aliceInWonderland.json';
-import theCantervilleGhost from './translations/theCantervilleGhost.json';
-import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import { useState } from 'react'
+import aliceInWonderland from './translations/aliceInWonderland.json'
+import theCantervilleGhost from './translations/theCantervilleGhost.json'
+import Button from '@mui/material/Button'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Popover from "@mui/material/Popover"
+import Typography from '@mui/material/Typography'
 import './App.css';
+
 
 type Book = {
   title: 'Alice in Wonderland',
@@ -29,6 +31,7 @@ type Paragraph = {
 const App = () => {
   const [bookJson, setBookJson] = useState<Paragraph[]>(aliceInWonderland)
   const [page, setPage] = useState<number>(0)
+  const [showPopover, setShowPopover] = useState<boolean>(false)
   const [textLanguage, setTextLanguage] = useState<Language | null>('English')
   const [popUpLanguage, setPopupLanguageLanguage] = useState<Language | null>('French')
 
@@ -39,13 +42,32 @@ const App = () => {
       <>
         <Card>
           <CardContent>
-            <Typography variant="body2">
-              {bookJson[page][textLanguage]}
-            </Typography>
+            <span 
+              id='card-content-span'
+              onClick={() => setShowPopover(!showPopover)}
+            >
+              <Typography variant="body2">
+                {bookJson[page][textLanguage]}
+              </Typography>
+              <Popover
+                id='translation-popover'
+                open={showPopover}
+                anchorEl={document.getElementById('card-content-span')}
+                onClose={() => setShowPopover(false)}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                transformOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+              >
+                {bookJson[page][popUpLanguage]}
+              </Popover>
+            </span>
           </CardContent>
         </Card>
-        {/* <p>Text Language: {bookJson[page][textLanguage]}</p>
-        <p>Popup Language: {bookJson[page][popUpLanguage]}</p> */}
       </>
       }
       <Button 
