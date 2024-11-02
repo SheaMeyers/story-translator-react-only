49fbe0e40ade038b2c8b4987841dc9c6348ab587
Shea Meyers <shea.meyers@gmail.com>
2024-11-02 17:58:47 +0100

    Move modal to it's own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
 import { useState } from "react";
-import aliceInWonderland from "./translations/aliceInWonderland.json";
-import theCantervilleGhost from "./translations/theCantervilleGhost.json";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
-import InputLabel from "@mui/material/InputLabel";
-import Modal from "@mui/material/Modal";
-import MenuItem from "@mui/material/MenuItem";
 import Popover from "@mui/material/Popover";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
-import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import {
   getSelectedBookCookie,
@@ -22,27 +15,27 @@ import {
   setSelectedTextLanguageCookie,
 } from "./cookies";
 import { BookTitles, Language, Paragraph } from "./types";
-import { Languages } from "./constants";
+import { Books } from "./constants";
 import "./App.css";
-
-const Books = {
-  "Alice in Wonderland": aliceInWonderland,
-  "The Canterville Ghost": theCantervilleGhost,
-};
+import SelectorModal from "./SelectorModal";
 
 const App = () => {
-  const [bookJson, setBookJson] = useState<Paragraph[] | null>(
-    Books[getSelectedBookCookie()] || null
+  const [bookJson, setBookJson] = useState<Paragraph[]>(
+    Books[getSelectedBookCookie()] || []
   );
   const [page, setPage] = useState<number>(getSelectedPageCookie() || 0);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
   const [showPopover, setShowPopover] = useState<boolean>(false);
+
   const [textLanguage, setTextLanguage] = useState<Language | null>(
     getSelectedTextLanguageCookie() || null
   );
   const [popUpLanguage, setPopupLanguage] = useState<Language | null>(
     getSelectedPopupLanguageCookie() || null
   );
+
+  const closeModal = () => setIsModalOpen(false);
 
   const updateBookJson = (bookTitle: BookTitles) => {
     setBookJson(Books[bookTitle]);
@@ -78,86 +71,18 @@ const App = () => {
         )}
       </Button>
 
-      <Modal
-        open={isModalOpen}
-        onClose={() => setIsModalOpen(false)}
-        aria-labelledby="book-language-selector"
-        aria-describedby="choose-language-and-books"
-      >
-        <Card>
-          <CardContent>
-            <div className="Modal__Chooser">
-              <InputLabel id="book-select-label">Choose Book</InputLabel>
-              <Select
-                labelId="book-select-label"
-                id="book-select"
-                value={Object.keys(Books).find(
-                  (key) => Books[key as BookTitles] === bookJson
-                )}
-                label="Age"
-                onChange={(event: SelectChangeEvent) =>
-                  updateBookJson(event.target.value as BookTitles)
-                }
-              >
-                {Object.keys(Books).map((book) => (
-                  <MenuItem value={book}>{book}</MenuItem>
-                ))}
-              </Select>
-            </div>
-            <div className="Modal__Chooser">
-              <InputLabel id="book-language-select-label">Book Text</InputLabel>
-              <Select
-                labelId="book-language-select-label"
-                id="book-language-select"
-                value={textLanguage?.toString()}
-                label="Age"
-                onChange={(event: SelectChangeEvent) =>
-                  updateTextLanguage(event.target.value as Language)
-                }
-              >
-                {Languages.map((language) => (
-                  <MenuItem value={language}>{language}</MenuItem>
-                ))}
-              </Select>
-            </div>
-            <div className="Modal__Chooser">
-              <InputLabel id="popup-language-select-label">
-                Popup Text
-              </InputLabel>
-              <Select
-                labelId="popup-language-select-label"
-                id="popup-language-select"
-                value={popUpLanguage?.toString()}
-                label="Age"
-                onChange={(event: SelectChangeEvent) =>
-                  updatePopupLanguage(event.target.value as Language)
-                }
-              >
-                {Languages.map((language) => (
-                  <MenuItem value={language}>{language}</MenuItem>
-                ))}
-              </Select>
-            </div>
-
-            {bookJson && (
-              <div className="Modal__Chooser">
-                <InputLabel id="current-page-label">Current Page</InputLabel>
-                <TextField
-                  id="current-page-input"
-                  variant="outlined"
-                  type="number"
-                  value={page + 1}
-                  onChange={(e) => {
-                    const newPage = parseInt(e.target.value) - 1;
-                    if (newPage > -1 && newPage <= bookJson.length)
-                      setPage(newPage);
-                  }}
-                />
-              </div>
-            )}
-          </CardContent>
-        </Card>
-      </Modal>
+      <SelectorModal
+        isModalOpen={isModalOpen}
+        handleClose={closeModal}
+        bookJson={bookJson}
+        updateBookJson={updateBookJson}
+        textLanguage={textLanguage}
+        updateTextLanguage={updateTextLanguage}
+        popUpLanguage={popUpLanguage}
+        updatePopupLanguage={updatePopupLanguage}
+        page={page}
+        updatePage={updatePage}
+      />
 
       {bookJson && textLanguage && popUpLanguage && (
         <>
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,9 @@
+import aliceInWonderland from "./translations/aliceInWonderland.json";
+import theCantervilleGhost from "./translations/theCantervilleGhost.json";
+
 export const Languages = ["English", "Spanish", "French", "German"] as const;
+
+export const Books = {
+    "Alice in Wonderland": aliceInWonderland,
+    "The Canterville Ghost": theCantervilleGhost,
+  };
