1df78e1c6d639ad68c1a269013775dcae000473a
Shea Meyers <shea.meyers@gmail.com>
2024-11-16 13:23:51 +0100

    Updates

--- src/constants.ts
+++
@@ -1,9 +0,0 @@
-import aliceInWonderland from "./bookTranslations/aliceInWonderland.json";
-import theCantervilleGhost from "./bookTranslations/theCantervilleGhost.json";
-
-export const Languages = ["English", "Spanish", "French", "German"] as const;
-
-export const Books = {
-    "Alice in Wonderland": aliceInWonderland,
-    "The Canterville Ghost": theCantervilleGhost,
-  };
